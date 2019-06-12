dc003b3c45be39e70d49d15f95d9ba41a580f25f
Bluemi <bruno.schilling@protonmail.ch>
2019-06-12 21:20:21 +0200

    added nice rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 extern crate sfml;
 
-use sfml::graphics::RenderWindow;
+use sfml::graphics::{RenderWindow, RectangleShape, Color};
 use sfml::window::{Window, VideoMode, Style, Event};
+use crate::sfml::graphics::RenderTarget;
+use crate::sfml::graphics::Shape;
+use crate::sfml::graphics::Transformable;
 use std::thread;
 use std::time;
+use sfml::system::Vector2f;
 
 fn main() {
 	let desktop = VideoMode::desktop_mode();
-	let mut window = Window::new(desktop,
-								"SFML window",
-								Style::CLOSE,
-								&Default::default());
+	let mut window = RenderWindow::new(
+		desktop,
+		"SFML window",
+		Style::FULLSCREEN,
+		&Default::default()
+	);
+
+	let mut rectangle = RectangleShape::with_size(Vector2f::new(20.0, 40.0));
+	rectangle.set_position(Vector2f::new(50.0, 50.0));
+	rectangle.set_fill_color(&Color::WHITE);
 
 	while window.is_open() {
 		if let Some(Event::Closed) = window.poll_event() {
 			break;
 		}
+
+		window.draw(&rectangle);
+
 		window.display();
 		thread::sleep(time::Duration::from_millis(1));
     }
