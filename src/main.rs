2d2eb9ccefc15e08efd29c3118f7634a7ddbfe58
Bluemi <bruno.schilling@protonmail.ch>
2019-06-12 21:05:29 +0200

    added empty sfml window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate sfml;
+
+use sfml::graphics::RenderWindow;
+use sfml::window::{Window, VideoMode, Style};
+use std::thread;
+use std::time;
+
 fn main() {
-    println!("Hello, world!");
+    let desktop = VideoMode::desktop_mode();
+    let mut window = Window::new(desktop,
+                              "SFML window",
+                              Style::CLOSE,
+                              &Default::default());
+
+    while window.is_open() {
+        window.poll_event();
+        window.display();
+        thread::sleep(time::Duration::from_millis(1));
+    }
 }
