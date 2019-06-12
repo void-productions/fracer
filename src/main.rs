4a53831fb6b7d9ea1b0ffc19d3395e90f0313238
memoryleak47 <memoryleak47@protonmail.ch>
2019-06-12 21:08:49 +0200

    space -> tabs, you can now close the window

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 extern crate sfml;
 
 use sfml::graphics::RenderWindow;
-use sfml::window::{Window, VideoMode, Style};
+use sfml::window::{Window, VideoMode, Style, Event};
 use std::thread;
 use std::time;
 
 fn main() {
-    let desktop = VideoMode::desktop_mode();
-    let mut window = Window::new(desktop,
-                              "SFML window",
-                              Style::CLOSE,
-                              &Default::default());
+	let desktop = VideoMode::desktop_mode();
+	let mut window = Window::new(desktop,
+								"SFML window",
+								Style::CLOSE,
+								&Default::default());
 
-    while window.is_open() {
-        window.poll_event();
-        window.display();
-        thread::sleep(time::Duration::from_millis(1));
+	while window.is_open() {
+		if let Some(Event::Closed) = window.poll_event() {
+			break;
+		}
+		window.display();
+		thread::sleep(time::Duration::from_millis(1));
     }
 }
